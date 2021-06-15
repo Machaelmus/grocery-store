a746ad5ebafa2d192de8a04487e9018da7fa4c6c
Machaelmus <michaeljtufo@gmail.com>
2021-06-15 15:53:04 -0400

    store setup

--- src/index.js
+++ src/index.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import {Provider} from 'react-redux';
+import configureStore from './store';
 import './index.css';
 import App from './App';
 
+const store = configureStore();
+
+if(process.env.NODE_ENV !== 'production') {
+  window.store = store;
+}
+//When we're not in production environment, we can access the store via window
+
 function Root() {
   return (
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   );
 }
 
---
+++ src/store/index.js
@@ -0,0 +1,23 @@
+import { createStore, combineReducers, applyMiddleware, compose } from "redux";
+
+const configureStore = (preloadedState) => {
+    return createStore(rootReducer, preloadedState, enhancer);
+};
+
+
+const rootReducer = combineReducers({
+
+})
+
+let enhancer;
+
+if (process.env.NODE_ENV !== "production") {
+    const logger = require("redux-logger").default;
+    const composeEnhancers =
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+    enhancer = composeEnhancers(applyMiddleware(logger));
+}
+
+
+
+export default configureStore;
