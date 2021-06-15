853991aaf85fa4b3465c2b7070ee371ce657ac2c
cody <cody.brown1993@gmail.com>
2021-06-15 16:16:10 -0400

    normalized produceReducer POPULATE

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,12 @@
 import { createStore, combineReducers, applyMiddleware, compose } from "redux";
-
+import produceReducer from "./produce";
 const configureStore = (preloadedState) => {
     return createStore(rootReducer, preloadedState, enhancer);
 };
 
 
 const rootReducer = combineReducers({
-
+    produce: produceReducer
 })
 
 let enhancer;
@@ -14,7 +14,7 @@ let enhancer;
 if (process.env.NODE_ENV !== "production") {
     const logger = require("redux-logger").default;
     const composeEnhancers =
-    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+        window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
     enhancer = composeEnhancers(applyMiddleware(logger));
 }
 
